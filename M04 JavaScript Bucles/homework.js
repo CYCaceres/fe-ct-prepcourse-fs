a618e4512edcf89f459c44d3e0ca3642d21cbbac
CYCaceres <127443752+CYCaceres@users.noreply.github.com>
2023-03-31 16:56:41 -0300

    Ejercicio 4

--- M04 JavaScript Bucles/homework.js
+++ M04 JavaScript Bucles/homework.js
@@ -61,23 +61,51 @@ function colors(color) {
    // Si no es ninguno de esos colores           --> "Color not found".
    // IMPORTANTE: utilizar el statement SWITCH.
    // Tu código:
-   if (color === "blue" ) return "This is blue";
-   else if (color === "red") return "This is red";
-   else if (color === "green") return "This is green";
-   else if (color === "orange") return "This is orange";
-   else return "Color not found"; 
+
+   //if (color === "blue" ) return "This is blue";
+   //else if (color === "red") return "This is red";
+   //else if (color === "green") return "This is green";
+   //else if (color === "orange") return "This is orange";
+   //else return "Color not found"; 
+
+switch (color) {
+   case "blue":
+   return "This is blue";
+   break;
+   case "red":
+      return "This is red";
+      break;
+      case "green":
+         return "This is green";
+         break;
+         case "orange":
+   return "This is orange";
+   break;
+   default:
+      return "Color not found";
+      break; 
+}
+  
 }
 
 function esDiezOCinco(num) {
    // Retornar true si "num" es 10 o 5.
    // De lo contrario, retornar false.
    // Tu código:
+   if(num===5||num===10){
+      return true;
+   }
+   return false;
 }
 
 function estaEnRango(num) {
    // Retornar true si "num" es menor que 50 y mayor que 20.
    // De lo contrario, retornar false.
    // Tu código:
+   if(num<50&&num>20){
+      return true;
+   }
+   return false;
 }
 
 function esEntero(num) {
@@ -87,6 +115,10 @@ function esEntero(num) {
    // Ejemplo: (-10) ---> true
    // De lo contrario, retorna false.
    // Tu código:
+   if(num===Math.round(num)){
+      return true;
+   }
+   return false;
 }
 
 function fizzBuzz(num) {
@@ -95,6 +127,16 @@ function fizzBuzz(num) {
    // Si "num" es divisible entre 3 y 5 (ambos), retorna "fizzbuzz".
    // De lo contrario, retorna false.
    // Tu código:
+
+   // if(num%15===0){ 
+   if(num%3===0&&num%5===0){
+      return "fizzbuzz";
+   }else if(num%3===0){
+      return "fizz";
+   }else if(num%5===0){
+      return "buzz"
+   }
+   return false;
 }
 
 function operadoresLogicos(num1, num2, num3) {
@@ -105,6 +147,22 @@ function operadoresLogicos(num1, num2, num3) {
    // Si todos los argumentos son cero, retornar ---> "Error".
    // Si no se cumple ninguna de las condiciones anteriores, retornar false.
    // Tu código:
+   if(num1>num2&&num1>num3&&num1%2===0){
+      return "Numero 1 es mayor y positivo";
+   }
+   if(num1<0||num2<0||num3<0){
+      return "Hay negativos";
+   }
+   if(num3>num1&&num3>num2){
+      return num3 + 1;
+   }
+   if(num1===0&&num2===0&&num3===0){
+      return "Error";
+   }
+   else{
+      return false;
+   }
+
 }
 
 function esPrimo(num) {
@@ -114,18 +172,41 @@ function esPrimo(num) {
    // [Pista 2]: puedes resolverlo utilizando un `bucle for`.
    // [Nota]: los números negativos, 0 y 1 NO son números primos.
    // Tu código:
+   if(num <= 1){
+      return false;
+   }else{
+      // 5/2 5/3 5/4
+      for(let i = 2; i < num; i++){
+         if(num % i === 0){
+            return false;
+         }
+      }
+      return true;
+   }
 }
 
 function esVerdadero(valor) {
    // Si "valor" es verdadero retornar "Soy verdadero".
    // Caso contrario, retornar "Soy falso".
    // Tu código:
+   if(valor === true){
+      return "Soy verdadero";
+   }else{
+      return "Soy falso"
+   }
 }
 
 function tieneTresDigitos(num) {
    // Si el número recibido tiene tres dígitos retornar true.
    // Caso contrario, retornar false.
    // Tu código:
+
+   // if(num > 99 && num < 999){
+   if(num.toString().length === 3){
+      return true;
+   }else{
+      return false;
+   }
 }
 
 function doWhile(num) {
@@ -133,6 +214,13 @@ function doWhile(num) {
    // Retornar el valor final.
    // Utilizar el bucle Do-While.
    // Tu código:
+   let iterador = 8;
+   let suma = num;
+   do{
+      suma +=5;
+      iterador--;
+   }while(iterador > 0)
+   return suma;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
